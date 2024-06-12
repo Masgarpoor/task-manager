d89e59e631ab83017b50d74a8a5d0f6e55c182dc
mohammad <pycodemaestro@gmail.com>
2024-06-12 15:47:23 +0330

    Add task and Update task API

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -136,4 +136,96 @@ export default class TaskController {
       });
     }
   }
+
+  static addTask(req, res) {
+    const title = req.body.title;
+    const completed = req.body.isCompleted ? true : false;
+
+    if (title) {
+      if (title.length < 3) {
+        return res.status(400).send({
+          success: false,
+          body: null,
+          message: "Title must contains 3 characters or more",
+        });
+      } else if (Task.getTaskByTitle(title)) {
+        return res.status(409).json({
+          success: false,
+          body: null,
+          message: "A task already exists with this title",
+        });
+      }
+      try {
+        const task = new Task(title, completed);
+        task.save();
+        res.status(201).json({
+          success: true,
+          body: task,
+          message: "Task created",
+        });
+      } catch (e) {
+        res.status(500).josn({
+          success: false,
+          body: null,
+          message: "Internal Server Error",
+        });
+      }
+    } else {
+      res.status(400).send({
+        success: false,
+        body: null,
+        message: "Please send new task title.",
+      });
+    }
+  }
+
+  static updateTask(req, res) {
+    const { id } = req.params;
+    const { title, completed } = req.body;
+
+    if (completed !== undefined && title) {
+      if (title.length < 3) {
+        return res.status(400).send({
+          success: false,
+          body: null,
+          message: "Title must contains 3 characters or more",
+        });
+      }
+
+      let task = Task.getTaskByTitle(title);
+      if (task && task.id != id) {
+        return res.status(409).json({
+          success: false,
+          body: null,
+          message: "A task already exists with this title",
+        });
+      }
+
+      task = Task.getTaskById(Number(id));
+      if (task) {
+        try {
+          task.title = title;
+          task.completed = completed;
+          task.save();
+          res.json({
+            success: true,
+            body: task,
+            message: "Task updated",
+          });
+        } catch (error) {
+          res.status(500).json({
+            success: false,
+            body: null,
+            message: "Internal Server Error",
+          });
+        }
+      }
+    } else {
+      res.status(400).json({
+        success: false,
+        body: null,
+        message: "Please enter the title and copmleted",
+      });
+    }
+  }
 }
--- routes/task-routes.js
+++ routes/task-routes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.get("/tasks", TaskController.getAllTasks);
 router.get("/tasks/:id", TaskController.getTaskById);
+router.post("/tasks", TaskController.addTask)
+router.put('/tasks/:id', TaskController.updateTask)
 
 export default router;
