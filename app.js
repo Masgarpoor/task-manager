21f45a1cf167a2b81c9154c4137e3afdd7e5de50
mohammad <pycodemaestro@gmail.com>
2024-06-12 11:54:26 +0330

    Change toggle, edit, delete button based on API and Starting to apply the RESTful API standard

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import express from "express";
 
 import getRoutes from "./routes/get-routes.js";
 import postRoutes from "./routes/post-routes.js";
+import taskRoutes from "./routes/task-routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,14 +14,12 @@ const staticPath = path.join(__dirname, "static");
 const app = express();
 
 app.use(express.static(staticPath));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
 
 app.use(getRoutes);
 app.use(postRoutes);
+app.use(taskRoutes)
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
